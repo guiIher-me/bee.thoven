56eaa7000cd3ccf4978c67db3e0772a126e67efd
Guilherme Oliveira <coliveira.guilherme@gmail.com>
2021-12-10 15:48:15 -0300

    feat: testing zenvia whatsapp bot v.1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const dotenv = require('dotenv');
-const { Client, TextContent, WebhookController } = require('@zenvia/sdk');
+const got = require('got');
+const FormData = require('form-data');
+
+const { Client, FileContent, TextContent, WebhookController } = require('@zenvia/sdk');
 
 dotenv.config();
 
@@ -8,21 +11,70 @@ const client = new Client(process.env.ZENVIA_TOKEN);
 const whatsapp = client.getChannel('whatsapp');
 
 const webhook = new WebhookController({
-  messageEventHandler: (messageEvent) => {
-    console.log('Message event:', messageEvent);
+  channel: 'whatsapp',
+  messageEventHandler: async (messageEvent) => {
+    let content = [new TextContent('Testado')];
 
-    const content = new TextContent('Testado');
+    if (messageEvent.message.contents[0].type === 'file' && messageEvent.message.contents[0].fileMimeType.includes('audio')) {
+      const music = await recognizeMusic(messageEvent.message.contents[0].fileUrl);
 
-    whatsapp.sendMessage(messageEvent.message.to, messageEvent.message.from, content)
-    .then(response => {
-      console.log('Response:', response);
+      if (music) {
+        let text = '';
+        if (music.artist) {
+          text = `${text}Artista: *${music.artist}*\n`;
+        }
+        if (music.title) {
+          text = `${text}Título: *${music.title}*\n`;
+        }
+        if (music.album) {
+          text = `${text}Álbum: *${music.album}*\n`;
+        }
+        content = [new TextContent(text)];
+        if (music.deezer && music.deezer.picture) {
+          content.push(new FileContent(music.deezer.picture, 'image/jpeg'));
+        }
+        if (music.deezer && music.deezer.preview) {
+          content.push(new FileContent(music.deezer.preview, 'audio/mpeg'));
+        }
+      } else {
+        content = [new TextContent('Não foi possível identificar a música do áudio.')];
+      }
+    }
+
+    whatsapp.sendMessage(messageEvent.message.to, messageEvent.message.from, ...content)
+    .then((response) => {
+      console.debug('Response:', response);
     });
   },
-  channel: 'whatsapp',
 });
 
 webhook.on('listening', () => {
-  console.log('Webhook is listening');
+  console.info('Webhook is listening');
 });
 
 webhook.init();
+
+const recognizeMusic = async (url) => {
+  const form = new FormData();
+  form.append('api_token', process.env.AUDD_TOKEN);
+  form.append('url', url);
+  form.append('return', 'deezer');
+
+  const response = await got.post('https://api.audd.io/', {
+    body: form,
+    responseType: 'json',
+    resolveBodyOnly: true,
+  });
+
+  if (response && response.result) {
+    return {
+      artist: response.result.artist,
+      title: response.result.title,
+      album: response.result.album,
+      deezer: {
+        picture: response.result.deezer && response.result.deezer.artist ? response.result.deezer.artist.picture_medium : undefined,
+        preview: response.result.deezer ? response.result.deezer.preview : undefined,
+      },
+    };
+  }
+};
