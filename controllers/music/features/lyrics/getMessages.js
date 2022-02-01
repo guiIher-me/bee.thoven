af50d125d4a9587848c356f2fe3c6a58ef179f68
Guilherme Oliveira <coliveira.guilherme@gmail.com>
2022-02-01 10:13:58 -0300

    refactor: remove html coding from lyrics

--- controllers/music/features/lyrics/getMessages.js
+++ controllers/music/features/lyrics/getMessages.js
@@ -3,9 +3,10 @@ const MESSAGES = require('../../../message/messages.enum')
 
 async function getMessages(music) {
     const content = []
-    if (music.lyrics)
-        content.push(MessageHelper.toText(music.lyrics))
-    else
+    if (music.lyrics) {
+        const lyrics = music.lyrics.replace(/&#39;/g, ' ').replace(/&#34;/g, '"')
+        content.push(MessageHelper.toText(lyrics))
+    } else
         content.push(MessageHelper.toText(MESSAGES.ERROR_LYRICS_NOT_FOUND))
 
     return content
